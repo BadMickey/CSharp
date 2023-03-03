16223328f77035d972e9d5c579534ffb6e3846a1
BadMickey <joaovitorcampos161@gmail.com>
2023-03-03 13:59:17 -0300

    Minha primeira API - Uma API simples com os métodos GET, POST, PUT, DELETE. - Usando o SQL Server

---
+++ MinhaPrimeiraAPI/MinhaPrimeiraAPI/Data/SistemasTarefasBDContex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaPrimeiraAPI.Data.Map;
+using MinhaPrimeiraAPI.Models;
+
+namespace MinhaPrimeiraAPI.Data
+{
+    public class SistemasTarefasBDContex : DbContext
+    {
+        public SistemasTarefasBDContex(DbContextOptions<SistemasTarefasBDContex> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MinhaPrimeiraAPI/MinhaPrimeiraAPI/Repositorios/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using MinhaPrimeiraAPI.Models;
+
+namespace MinhaPrimeiraAPI.Repositorios.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        Task<List<TarefaModel>> BuscarTodasTarefas();
+        Task<TarefaModel> BuscarPorID(int id);
+        Task<TarefaModel> Adicionar(TarefaModel tarefa);
+        Task<TarefaModel> Atualizar(TarefaModel tarefa, int id);
+        Task<bool> Apagar(int id);
+    }
+}
